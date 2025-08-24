b18accf9b75ce504562409c0463491f42bd71ef7
Alesia Vasilkova <leska.vasilkova@inbox.ru>
2025-08-25 00:16:00 +0200

    init: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const numbersTeens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const numberTys = [
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const hundreds = 'hundred';
+  if (number < 10) {
+    return numbers[number];
+  }
+
+  if (number < 20) {
+    return numbersTeens[number - 10];
+  }
+
+  const hundred = Math.floor(number / 100);
+  const digit = number % 10;
+  const tys = Math.floor((number % 100) / 10);
+
+  const hund1 =
+    hundred === 1 ? `one ${hundreds} ` : `${numbers[hundred]} ${hundreds} `;
+  const hunds = hundred ? hund1 : '';
+  const ten1 = tys === 1 ? `${numbersTeens[digit]} ` : `${numberTys[tys - 2]} `;
+  const tens = tys ? ten1 : '';
+  return `${hunds}${tens}${tys !== 1 && digit ? numbers[digit] : ''}`.trim();
 };
